bc19c7d2719f0a235afa8d00d0228779306abe93
sergey <adam.carraway@mail.ru>
2021-08-14 17:35:45 +0300

    ++

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
     ],
 
     /*
