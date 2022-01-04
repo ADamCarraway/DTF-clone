e3636aaf48444d85b4fcb1a58503e1d3c2655587
Sergey Serousov <adam.carraway@mail.ru>
2022-01-05 00:39:26 +0300

    ++

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'encrypted' => false,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
         ],
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -26,10 +26,10 @@ window.Echo = new Echo({
   broadcaster: 'pusher',
   key: process.env.MIX_PUSHER_APP_KEY,
   cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-  forceTLS: false,
+  disableStats: true,
+  forceTLS: true,
   wsHost: window.location.hostname,
   wsPort: 6001,
-  wssPort: 443
 });
 
 import '~/plugins'
