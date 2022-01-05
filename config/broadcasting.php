18718658223b91eb58bec99332365c8cd79b8e98
Sergey Serousov <adam.carraway@mail.ru>
2022-01-05 16:02:06 +0300

    pusher setting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster'   => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host'      => env('APP_URL'),
+                'host'      => env('PUSHER_APP_HOST', '127.0.0.1'),
                 'port'      => 6001,
                 'scheme'    => env('PUSHER_APP_SCHEME', 'https'),
             ],
