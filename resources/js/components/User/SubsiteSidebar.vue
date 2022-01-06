49b7b0cf762ac41634f8ed80475c1124ea249c99
Sergey Serousov <adam.carraway@mail.ru>
2022-01-06 19:10:09 +0300

    ++

--- resources/js/components/User/SubsiteSidebar.vue
+++ resources/js/components/User/SubsiteSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="l-page__sidebar lm-hidden" style="position: relative;">
-    <div class="subsite-sidebar" style="width: 300px; position: sticky; top: 140px;">
+    <div class="subsite-sidebar" style="width: 300px; position: sticky; top: 80px;">
       <subscribers-block :data="data"></subscribers-block>
       <user-categories-block v-if="data.type === 'user'" :data="data"></user-categories-block>
       <regulations-box v-if="data.regulations && data.type === 'category'" :data="data.regulations"></regulations-box>
